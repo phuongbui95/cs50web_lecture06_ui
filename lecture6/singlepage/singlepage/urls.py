10e134f16a8cbcae8d3376e3e2a13cbc69d695e1
PB Github <phuong.buitan95@gmail.com>
2023-05-20 17:01:45 +0700

    fix url pattern to fech api

--- lecture6/singlepage/singlepage/urls.py
+++ lecture6/singlepage/singlepage/urls.py
@@ -1,4 +1,4 @@
-"""lecture6 URL Configuration
+"""singlepage URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('singlepage/', include("singlepage.urls")),
-    path('', include("singlepage.urls")),
+    path('singlepage1/', include('singlepage1.urls')),
+    path('singlepage2/', include('singlepage2.urls')),
 ]
--- lecture6/singlepage/singlepage1/apps.py
+++ lecture6/singlepage/singlepage1/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SinglepageConfig(AppConfig):
+class Singlepage1Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'singlepage'
+    name = 'singlepage1'
